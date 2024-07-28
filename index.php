e0b4643125a0af1ba7b4e117d1b049dd68fcd20b
rutuja091 <gaikwadrutuja254@gmail.com>
2024-07-28 10:42:05 +0530

    add slider

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="./index.css">
    
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-   
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
     <!-- Header -->
@@ -25,7 +25,56 @@
 
     <!-- Main Content -->
     <main>
-        <!-- Add your main content here -->
+      
+<div id="movieCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#movieCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#movieCarousel" data-slide-to="1"></li>
+        <li data-target="#movieCarousel" data-slide-to="2"></li>
+        <li data-target="#movieCarousel" data-slide-to="3"></li>
+        <li data-target="#movieCarousel" data-slide-to="4"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active" style="background-image:url('Images/banner1.jpg');">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Movie Title 1</h5>
+                <p>Brief description of the movie.</p>
+            </div>
+        </div>
+        <div class="carousel-item" style="background-image: url('Images/banner2.jpg');">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Movie Title 2</h5>
+                <p>Brief description of the movie.</p>
+            </div>
+        </div>
+        <div class="carousel-item" style="background-image: url('Images/banner3.jpg');">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Movie Title 3</h5>
+                <p>Brief description of the movie.</p>
+            </div>
+        </div>
+        <div class="carousel-item" style="background-image: url('Images/banner4.jpg');">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Movie Title 4</h5>
+                <p>Brief description of the movie.</p>
+            </div>
+        </div>
+        <div class="carousel-item" style="background-image: url('Images/banner5.jpg');">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Movie Title 5</h5>
+                <p>Brief description of the movie.</p>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#movieCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#movieCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
     </main>
 
     <footer class="footer">
@@ -46,6 +95,10 @@
   <!-- Font Awesome CDN for social media icons -->
   <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="script.js"></script>
 </body>
 </html>
