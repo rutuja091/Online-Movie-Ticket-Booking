43e8f831fafc85cbc41785ddd844ee252ca5acbc
NikitaNimse <nimsenikita710>
2024-08-08 15:13:13 +0530

    footer added

--- User/pages/Drama.php
+++ User/pages/Drama.php
@@ -5,24 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie Ticket Booking</title>
     <link rel="stylesheet" href="./../css/drama.css">
-    <link rel="stylesheet" href="./../../common/header-footer.css">
+    <link rel="stylesheet" href="./../css/contact.css">
    
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <!--- Header --->
-    <header class="header">
-        <div class="logo"><img src="./../../Images/logo2.png" class="logo-img"></div>
-        <nav class="nav">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Movies</a></li>
-            </ul>
-        </nav>
-    </header>
+    <!-- Header -->
+    
+    <nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <div class="logo"><img src="./../../Images/logo2.png" class="logo-img" alt="Logo"></div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Movies</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            View More
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">About Us</a></li>
+            <li><a class="dropdown-item" href="#">Contact Us</a></li>
+            <li><a class="dropdown-item" href="#">Gallery</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div class="d-flex align-items-center">
+      <a href="#" class="btn btn-outline-primary">
+        <i class="bi bi-person"></i> Login
+      </a>
+    </div>
+  </div>
+</nav>
+
+<!-- end nav -->
 
 
     <div class="centered-content">
@@ -83,14 +108,18 @@
 
 
 
-     <!-- Footer -->
-<div class="footer">
+   <!-- Footer -->
+ <div class="footer">
     <div class="container">
         <div class="row">
+     
             <div class="col-md-4">
+              <div class="logo" style="width:10px; margin-left:5px;">
+              <img src="./../../Images/logo2.png" class="logo-img" alt="Logo">
+              </div>
                 <h3 class="footer-title">STARLIGHT CINEMA</h3>
-                <p>Buy movie tickets easily with Aovis system nationwide</p>
-                <a href=".\movie-details.php" class="btn btn-warning">Get Your Ticket</a>
+                <p>Buy movie tickets easily with starlight system nationwide</p>
+                <a href="#" class="btn btn-warning">visit now</a>
             </div>
             <div class="col-md-2">
                 <h5 class="footer-title">Movies</h5>
@@ -106,8 +135,8 @@
                 <h5 class="footer-title">Links</h5>
                 <ul class="list-unstyled">
                     <li><a href="#">About</a></li>
-                    <li><a href="#">My Account</a></li>
-                    <li><a href="#">News</a></li>
+                    <li><a href="#">My gallery</a></li>
+                    <li><a href="#">Movies</a></li>
                     <li><a href="#">Latest Events</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
@@ -115,7 +144,7 @@
                 
             <div class="col-md-4">
                 <h5 class="footer-title">Newsletter</h5>
-                <p>Subscribe to Leitmotif newsletter this very day.</p>
+                <p>Subscribe to starlight channel.</p>
                 <div class="newsletter">
                     <input type="email" class="form-control" placeholder="Email Address">
                     <button class="btn btn-warning mt-2">Subscribe</button>
@@ -133,11 +162,10 @@
             </div>
         </div>
         <div class="text-center mt-4">
-            <p>&copy; Copyright 2023 by starlight.com</p>
+            <p>&copy; Copyright 2023 by Ovatheme.com</p>
         </div>
     </div>
 </div>
-
 
 
 <!-- Font Awesome for social media icons -->
--- User/pages/contact.php
+++ User/pages/contact.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie Ticket Booking</title>
-    <link rel="stylesheet" href="./../../common/header-footer.css">
+   
     <link rel="stylesheet" href="./../css/contact.css">
 
-    <link rel="stylesheet" href="./../css/movies.css">
    
    
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
@@ -17,35 +16,42 @@
 </head>
 <body>
     <!-- Header -->
-    <nav class="navbar">
-        <div class="navbar-logo">
-            <img src=".\..\..\Images\logo2.png" alt="Logo">
-        </div>
-        <ul class="navbar-links">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Movies</a></li>
-            <li class="nav-item dropdown  ps-5">
-            <a class="nav-link txt dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Subjects
-                      </a>
-                      <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="./feedback.php" target="_blank">contact</a></li>
-                          <li><a class="dropdown-item" href="./contact.php" target="_blank" >feedback </a></li>
-                          <li><a class="dropdown-item" href="./gallery.php" target="_blank" >gallery</a></li>
-                          <li><a class="dropdown-item" href="./trailer4.php" target="_blank">trailer</a></li>
-                          
-                        </ul>
-                    </li>
+    
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <div class="logo"><img src="./../../Images/logo2.png" class="logo-img" alt="Logo"></div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Movies</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            View More
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">About Us</a></li>
+            <li><a class="dropdown-item" href="#">Contact Us</a></li>
+            <li><a class="dropdown-item" href="#">Gallery</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div class="d-flex align-items-center">
+      <a href="#" class="btn btn-outline-primary">
+        <i class="bi bi-person"></i> Login
+      </a>
+    </div>
+  </div>
+</nav>
 
-                   
-
-
-        </ul>
-        <div class="navbar-login">
-        <img src="./../../Images/User-images/login.png" alt="Logo" style="height:50px;cursor:pointer">
-        </div>
-    </nav>
+<!-- end nav -->
   
    <div class="bodycontent">
 
@@ -96,31 +102,20 @@
         <p>123 Movie Street<br>Film City, 414001<br></p>
         <a href="https://www.google.com/maps/search/starlight+cinema/@18.9602201,72.6499553,10.09z?entry=ttuF" target="_blank">View on Map</a>
     </div>
-  
-
-       <img src="./../../Images/User-images/starmap.png" class="map" >
-
-
+      <img src="./../../Images/User-images/starmap.png" class="map" >
 
 </div>
   
     
-  
-    
-
- 
-
-
-  <!-- Footer -->
-<div class="footer">
+ <!-- Footer -->
+ <div class="footer">
     <div class="container">
         <div class="row">
+     
             <div class="col-md-4">
-              
-            <div class="navbar-logo">
-            <img src=".\..\..\Images\logo2.png" alt="Logo">
-        </div>
-                 
+              <div class="logo" style="width:10px; margin-left:80px;">
+              <img src="./../../Images/logo2.png" class="logo-img" alt="Logo">
+              </div>
                 <h3 class="footer-title">STARLIGHT CINEMA</h3>
                 <p>Buy movie tickets easily with starlight system nationwide</p>
                 <a href="#" class="btn btn-warning">visit now</a>
@@ -138,19 +133,20 @@
             <div class="col-md-2">
                 <h5 class="footer-title">Links</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#">home</a></li>
                     <li><a href="#">About</a></li>
+                    <li><a href="#">My gallery</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Latest Events</a></li>
                     <li><a href="#">Contact</a></li>
-                    <li><a href="#"> movie Gallery</a></li>
-                   
                 </ul>
                 </div>
+                
             <div class="col-md-4">
-                <h5 class="footer-title">Address</h5>
-                <p class="info">123 movie streat,</p>
-                <p class="info">film city 414006</p>
+                <h5 class="footer-title">Newsletter</h5>
+                <p>Subscribe to starlight channel.</p>
                 <div class="newsletter">
-                   
+                    <input type="email" class="form-control" placeholder="Email Address">
+                    <button class="btn btn-warning mt-2">Subscribe</button>
                 </div>
                 <div class="mt-2">
                     <input type="checkbox" id="terms">
@@ -165,14 +161,20 @@
             </div>
         </div>
         <div class="text-center mt-4">
-            <p> &copy;Copyright 2023 by Starlight cinema.com</p>
+            <p>&copy; Copyright 2023 by Ovatheme.com</p>
         </div>
     </div>
 </div>
 
+
   <!-- Font Awesome CDN for social media icons -->
   <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="script.js"></script>
     <script src="script.js"></script>
 </body>
 </html>
