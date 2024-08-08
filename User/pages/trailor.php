8ac30d1e352df66553ac4d0d232956ab5b0d634a
rutuja091 <gaikwadrutuja254@gmail.com>
2024-08-08 13:36:06 +0530

    add index page proper

--- User/pages/trailor.php
+++ User/pages/trailor.php
@@ -10,20 +10,42 @@
 </head>
 <body>
     <!--- Header --->
-    <header class="header">
-        <div class="logo"><img src=".\..\..\Images\logo2.png" class="logo-img"></div>
-        <nav class="nav">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Movies</a></li>
+  
+    <nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <div class="logo"><img src="./Images/logo2.png" class="logo-img" alt="Logo"></div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Movies</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            View More
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">About Us</a></li>
+            <li><a class="dropdown-item" href="#">Contact Us</a></li>
+            <li><a class="dropdown-item" href="#">Gallery</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div class="d-flex align-items-center">
+      <a href="#" class="btn btn-outline-primary">
+        <i class="bi bi-person"></i> Login
+      </a>
+    </div>
+  </div>
+</nav>
 
-                <li><a href="#">Offers</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Contact Us</a></li>
-                <li><a href="#">Login</a></li>
-            </ul>
-        </nav>
-    </header>
+
 
     <!--- Main Content --->
     <main>
