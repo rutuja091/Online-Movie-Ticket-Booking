68263317f2a2089f715b6c736425f96ba1fbebc9
rutuja091 <gaikwadrutuja254@gmail.com>
2024-08-09 22:45:42 +0530

    add login page

--- Admin/pages/login.php
+++ Admin/pages/login.php
@@ -4,34 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie Ticket Booking</title>
-    <link rel="stylesheet" href="./../../common/header-footer.css">
-    <link rel="stylesheet" href="./../css/login.css">
+   
+    <link rel="stylesheet" href=" ./../css/login.css">
    
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-   
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <!-- Header -->
-    <header class="header">
-        <div class="logo"><img src=".\..\..\Images\logo2.png" class="logo-img"></div>
-        <nav class="nav">
-        <ul>
-            <li><a href="./admin-dashboard.php" style="margin-right:50px; font-size:30px;" >Home</a></li>
-            
-            <div class="d-flex align-items-center">
-            <a href="./login.php" class="btn btn-outline-primary">
-        <i class="bi bi-person"></i><img src =".\..\..\Images\login.png" style="height:40px;"> 
+
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <div class="logo"><img src="./../../Images/logo2.png" class="logo-img" alt="Logo"></div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+ <!--<li class="nav-item">
+          <a class="nav-link active" aria-current="page" href=".\..\..\index1.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href=".\..\..\User\pages\movies.php">Movies</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            View More
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href=".\..\..\User\pages\about.php">About Us</a></li>
+            <li><a class="dropdown-item" href=".\..\..\User\pages\contact.php">Contact Us</a></li>
+            <li><a class="dropdown-item" href=".\..\..\User\pages\gallery.php">Gallery</a></li>
+          </ul>
+        </li> -->
+      </ul>
+    </div>
+    <div class="d-flex align-items-center">
+      <a href="#" class="btn btn-outline-primary">
+      <i class="bi bi-person"></i> <img src =".\..\..\Images\login.png" style="height:40px;">
       </a>
     </div>
-            </ul>
-        </nav>
-    </header>
+  </div>
+</nav>
 
-     <!-- Main Content -->
-     <main>
-    <div class="container">
-        <form class="login-form">
-            <h2>Login</h2>
+
+
+    <div class="carousel-item active" style="background-image:url('./../../Images/User-images/login-bg.jpg');">
+
+    <div class="containers1">
+       
+        <form  class="login-form">
+        <h2>Login</h2>
             <div class="form-group">
                 <label for="username">Username</label>
                 <input type="text" id="username" name="username" required>
@@ -43,29 +66,92 @@
             </div>
             
             <button type="submit">Login</button>
+            <h6 style="color: #f3f1f1; margin-top: 20px; display: inline;">
+             if not have account &nbsp;
+           </h6>
+          <a href="./register.php" style="color: white; text-decoration: none; display: inline; margin-left: 5px;">
+            Register
+            </a>
         </form>
-    </div>
-    </main>
+    </div> 
 
-    <footer class="footer">
-        <div class="social-icons">
-            <a href="https://facebook.com" target="_blank"><i class="fa fa-facebook"></i></a>
-            <a href="https://twitter.com" target="_blank"><i class="fa fa-twitter"></i></a>
-            <a href="https://instagram.com" target="_blank"><i class="fa fa-instagram"></i></a>
-            <a href="https://linkedin.com" target="_blank"><i class="fa fa-linkedin"></i></a>
+   <!-- Footer -->
+   <div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3 class="footer-title">STARLIGHT CINEMA</h3>
+                <p>Buy movie tickets easily with Starlight Cinema system nationwide</p>
+                <a href="#" class="btn btn-warning">Get Your Ticket</a>
+            </div>
+            <div class="col-md-2">
+                <h5 class="footer-title">Movies</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">Comedy Shows</a></li>
+                    <li><a href="#">Drama</a></li>
+                    <li><a href="#">Kids Movie</a></li>
+                    <li><a href="#">Marathi Movies</a></li>
+                    <li><a href="#">Hindi Movies</a></li>
+                    <li><a href="#">English Movies</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h5 class="footer-title">Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+                </div>
+                
+            <div class="col-md-4">
+                <h5 class="footer-title">Newsletter</h5>
+                <p>Subscribe to STARLIGHT CINEMA .</p>
+                <div class="newsletter">
+                    <input type="email" class="form-control" placeholder="Email Address">
+                    <button class="btn btn-warning mt-2">Subscribe</button>
+                </div>
+                <div class="mt-2">
+                    <input type="checkbox" id="terms">
+                    <label for="terms">I agree to all terms and policies of the company</label>
+                </div>
+                <div class="social-icons mt-3">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
         </div>
-        <div class="links">
-            <a href="#home">Home</a>
-            <a href="#about">About Us</a>
-            <a href="#services">Services</a>
-            <a href="#contact">Contact Us</a>
+        <div class="text-center mt-4">
+            <p>&copy; Copyright 2024 by starlightcinema.com  &nbsp; &nbsp; &nbsp; &nbsp;    Developed By Gaikwad Rutuja & Nimse Nikita</p>
+          
         </div>
-        <p>&copy; 2024 Movie Ticket Booking System. All rights reserved.</p>
-    </footer>
+    </div>
+</div>
+
+
+
+<!-- Font Awesome for social media icons -->
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
   <!-- Font Awesome CDN for social media icons -->
   <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 
-    <script src="script.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script>  document.querySelector('.nav').addEventListener('click', function() {
+            this.querySelector('ul').classList.toggle('show');
+        });</script>
 </body>
 </html>
 
