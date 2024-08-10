d02ef83a5f96c8060a7c498a7e6552ed0989f1da
NikitaNimse <nimsenikita710>
2024-08-10 21:00:52 +0530

    update nav of about

--- User/pages/about.php
+++ User/pages/about.php
@@ -16,31 +16,29 @@
    
 </head>
 <body>
-    <!-- Header -->
+   <!-- Header -->
     
-<nav class="navbar navbar-expand-lg navbar-light">
+   <nav class="navbar navbar-expand-lg navbar-light">
   <div class="container-fluid">
-    <div class="logo">
-      <img src="./../../Images/logo2.png" class="logo-img" alt="Logo">
-  </div>
+    <div class="logo"><img src="./../../Images/logo2.png" class="logo-img" alt="Logo"></div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
       <ul class="navbar-nav mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="">Home</a>
+          <a class="nav-link active" aria-current="page" href=".\..\..\index1.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="#">Movies</a>
+          <a class="nav-link" href=".\..\..\User\pages\movies.php">Movies</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             View More
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <li><a class="dropdown-item" href=".\about.php">About Us</a></li>
-            <li><a class="dropdown-item" href=".\..\..\User\pages\contact.php">Contact Us</a></li>
+          <li><a class="dropdown-item" href=".\..\..\User\pages\about.php">About Us</a></li>
+            <li><a class="dropdown-item" href=".\contact.php">Contact Us</a></li>
             <li><a class="dropdown-item" href=".\..\..\User\pages\gallery.php">Gallery</a></li>
           </ul>
         </li>
@@ -230,18 +228,14 @@
 
 
 
-<!-- Font Awesome for social media icons -->
-<script src="https://kit.fontawesome.com/a076d05399.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-
   <!-- Font Awesome CDN for social media icons -->
   <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 
 
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="script.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
