2773a106292550b778fdf2495b8f34a50ce0acb5
rutuja091 <gaikwadrutuja254@gmail.com>
2024-08-10 18:22:53 +0530

    add registration page in admin side

--- Admin/pages/registration.php
+++ Admin/pages/registration.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie Ticket Booking</title>
-
+    <link rel="stylesheet" href=" ./../css/gallery.css">
+    <link rel="stylesheet" href=" ./../css/registration.css">
+   
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -19,7 +21,7 @@
     </button>
     <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
       <ul class="navbar-nav mb-2 mb-lg-0">
-      <!-- <li class="nav-item">
+      <li class="nav-item">
           <a class="nav-link active" aria-current="page" href=".\..\..\index1.php">Home</a>
         </li>
         <li class="nav-item">
@@ -34,7 +36,7 @@
             <li><a class="dropdown-item" href=".\..\..\User\pages\contact.php">Contact Us</a></li>
             <li><a class="dropdown-item" href=".\..\..\User\pages\gallery.php">Gallery</a></li>
           </ul>
-        </li> -->
+        </li>
       </ul>
     </div>
     <div class="d-flex align-items-center">
@@ -46,6 +48,9 @@
 </nav>
 
 
+
+    <div class="carousel-item active" style="background-image:url('./../../Images/User-images/login-bg.jpg');">
+    <div class="containers2">
         <form class="registration-form">
             <h2>Register</h2>
             <div class="form-group">
@@ -76,38 +81,43 @@
         </form>
     </div>
 
+   <!-- Footer -->
+   <div class="footer">
+    <div class="container">
         <div class="row">
             <div class="col-md-4">
                 <h3 class="footer-title">STARLIGHT CINEMA</h3>
-                <p>Buy movie tickets easily with Aovis system nationwide</p>
+                <p>Buy movie tickets easily with Starlight Cinema system nationwide</p>
                 <a href="#" class="btn btn-warning">Get Your Ticket</a>
             </div>
             <div class="col-md-2">
                 <h5 class="footer-title">Movies</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Adventure</a></li>
-                    <li><a href="#">Animation</a></li>
-                    <li><a href="#">Comedy</a></li>
-                    <li><a href="#">Crime</a></li>
+                    <li><a href="#">Comedy Shows</a></li>
+                    <li><a href="#">Drama</a></li>
+                    <li><a href="#">Kids Movie</a></li>
+                    <li><a href="#">Marathi Movies</a></li>
+                    <li><a href="#">Hindi Movies</a></li>
+                    <li><a href="#">English Movies</a></li>
                 </ul>
             </div>
             <div class="col-md-2">
                 <h5 class="footer-title">Links</h5>
                 <ul class="list-unstyled">
                     <li><a href="#">About</a></li>
-                    <li><a href="#">My Account</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Latest Events</a></li>
                     <li><a href="#">Contact</a></li>
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Login</a></li>
                 </ul>
                 </div>
+                
             <div class="col-md-4">
                 <h5 class="footer-title">Newsletter</h5>
-                <p>Subscribe to Leitmotif newsletter this very day.</p>
+                <p>Subscribe to STARLIGHT CINEMA .</p>
                 <div class="newsletter">
-                    <input type="email" class="form-control" placeholder="Email Address" style="width:350px">
-                    <button class="btn btn-warning mt-2" style="width:150px">Subscribe</button>
+                    <input type="email" class="form-control" placeholder="Email Address">
+                    <button class="btn btn-warning mt-2">Subscribe</button>
                 </div>
                 <div class="mt-2">
                     <input type="checkbox" id="terms">
@@ -122,7 +132,8 @@
             </div>
         </div>
         <div class="text-center mt-4">
-            <p> &copy;Copyright 2023 by Ovatheme.com</p>
+            <p>&copy; Copyright 2024 by starlightcinema.com  &nbsp; &nbsp; &nbsp; &nbsp;    Developed By Gaikwad Rutuja & Nimse Nikita</p>
+          
         </div>
     </div>
 </div>
