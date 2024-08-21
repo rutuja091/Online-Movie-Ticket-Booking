02b205da6b49956fadd11034d2a3c3711d2b5ab9
rutuja091 <gaikwadrutuja254@gmail.com>
2024-08-21 22:36:00 +0530

    add db con file

---
+++ User/pages/dbcon.php
@@ -0,0 +1,9 @@
+<?php
+$databaseHost = "localhost";
+$databaseName = "movieticketdb";
+$databaseUsername = "root";
+$databasePassword = "";
+
+//Database connection 
+
+$con = new mysqli($databaseHost, $databaseUsername, $databasePassword,$databaseName)or die($conn->connect_error());
