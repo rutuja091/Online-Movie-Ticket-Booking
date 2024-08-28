94cbd6c832829961b96c9fb10844115c70d1b9ce
NikitaNimse <nimsenikita710>
2024-08-28 22:30:43 +0530

    update reg

--- Admin/pages/update-registration.php
+++ Admin/pages/update-registration.php
@@ -5,26 +5,35 @@ $databaseUsername = "root";
 $databasePassword = "";
 
 //Database connection 
-$con = new mysqli($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-}
+$con = new mysqli($databaseHost, $databaseUsername, $databasePassword,$databaseName)or die($conn->connect_error());
+
 
 // Get id from URL parameter
+
 $id=$_GET['id'];
 
 // Select data associated with this particular id
-$sql = "SELECT * FROM user_registration WHERE id = $id";
-$query = $con->query($sql);
 
-// Fetch the next row of a result set as an associative array
-$row = $result->fetch_assoc();
+	$sql=  "SELECT * FROM user_registration WHERE id = $id";
 
-$user_name = $resultData['user_name'];
-$email = $resultData['email'];
-$password = $resultData['password'];
-$confirm_password = $resultData['confirm_password'];
+
+ 	$query= $con->query($sql);
+	// Fetch the next row of a result set as an associative array
+
+	$resultData = $query->fetch_assoc();
+
+    $user_name= $resultData['user_name'];
+
+   $email= $resultData['email'];
+
+   $password = $resultData['password'];
+	
+	$confirm_password = $resultData['confirm_password'];
+	
+
+
+	
 ?>
 
 <!DOCTYPE html>
@@ -32,115 +41,67 @@ $confirm_password = $resultData['confirm_password'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Registration</title>
-    <link rel="stylesheet" href=" ./../css/gallery.css">
-    <link rel="stylesheet" href=" ./../css/registration.css">
+    <title>Movie Ticket Booking</title>
+    <link rel="stylesheet" href="./../css/addmovie.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <!-- Header -->
+    <!--- Header --->
     <header class="header">
         <div class="logo"><img src=".\..\..\Images\logo2.png" class="logo-img"></div>
         <nav class="nav">
             <ul>
-                <li><a href="./admin-dashboard.php" style="margin-right:50px; font-size:30px;">Home</a></li>
+                <li><a href="#">Home</a></li>
+             
                 <div class="d-flex align-items-center">
-                    <a href="./login.php" class="btn btn-outline-primary">
-                        <i class="bi bi-person"></i><img src=".\\..\\..\\Images\\login.png" style="height:40px;">
-                    </a>
-                </div>
+            <a href="./login.php" class="btn btn-outline-primary">
+        <i class="bi bi-person"></i><img src =".\..\..\Images\login.png" style="height:40px;"> 
+      </a>
+    </div>
             </ul>
         </nav>
     </header>
 
-    <!-- Main Content -->
-    <main>
-        <div class="container">
-            <form class="registration-form" action="Update_registration.php?id=<?php echo $id; ?>" method="post" enctype="multipart/form-data">
-                <h2>Edit Registration</h2>
-                <div class="form-group">
-                    <label for="user_name" class="form-label">Username</label>
-                    <input type="text" id="user_name"
-                    placeholder="enter your user name" autocomplete="off" required="required"
-                     class="form-control" name="user_name" value="<?php echo $user_name; ?>" required>
-                </div>
-                <div class="form-group">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="email" id="email" 
-                    placeholder="enter your user email" autocomplete="off"
-                    class="form-control" name="email" value="<?php echo $email; ?>" required>
-                </div>
-                <div class="form-group">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" id="password"
-                     placeholder="password" autocomplete="off"
-                     class="form-control" name="password" value="<?php echo $password; ?>" required>
-                </div>
-                <div class="form-group">
-                    <label for="confirm_password" class="form-label">Confirm Password</label>
-                    <input type="password" id="confirm_password" 
-                     placeholder=" confirm  password" autocomplete="off"
-                    class="form-control" name="confirm_password" value="<?php echo $confirm_password; ?>" required>
-                </div>
-                <div class="mt-4 pt-2">
-                <input type="hidden"  class="bg-info py-2 px-3 border-0 " name="id"  value="<?php echo $id;?>"> 
-                </div>
 
-                <div class="mt-4 pt-2">
-                <input type="submit" value="Update" class="bg-info py-2 px-3 border-0 " name="user_registration" > 
-                </div>
-            </form>
+       <!-- Main Content -->
+       <div class="container1">
+    
+    <!--<form class="movie-form" action="/submit-movie" method="POST" enctype="multipart/form-data"> -->
+    <form action="" method="post" enctype="multipart/form-data">
+        <h2> Update Registration Details</h2>
+   
+        <div class="form-group">
+            <label for="user_name">Name:</label>
+            <input type="text" id="user_name" name="user_name" 
+            autocomplete="off" 
+            value="<?php  echo $user_name; ?>" required/>
         </div>
-
-        <?php
-        if (isset($_POST['user_registration'])) {
-            $id = $_POST['id'];
-            $user_name = $_POST['user_name'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            $confirm_password = $_POST['confirm_password'];
-
-            $sql = "UPDATE user_registration SET `user_name` = '$user_name', `email` = '$email', `password` = '$password', `confirm_password` = '$confirm_password' WHERE `id` = $id";
-
-            if ($con->query($sql)) {
-                echo '<script type="text/javascript">
-                        alert("Record Updated!!");
-                        window.location="manage-registration.php";
-                      </script>';
-            } else {
-                echo '<script type="text/javascript">
-                        alert("Record Not Updated");
-                        window.location="update-registration.php?id=$id";
-                      </script>';
-            }
-        }
-        ?>
-    </main>
-
-    <!-- Footer -->
-    <div class="footer">
-        <div class="container" style="background-color:black;">
-            <div class="row">
-                <!-- Footer content here -->
-            </div>
-            <div class="text-center mt-4">
-                <p>&copy;Copyright 2023 by Ovatheme.com</p>
-            </div>
+      
+        <div class="form-group">
+            <label for="email">Show Time:</label>
+            <input type="text" id="email" name="email" 
+            autocomplete="off" 
+            value="<?php  echo $email; ?>" required/>
         </div>
-    </div>
+        <div class="form-group">
+            <label for="password">password:</label>
+            <input type="password" id="password" name="password" 
+            autocomplete="off" 
+            value="<?php  echo $password; ?>" required />
+        </div>
+        <div class="form-group">
+              <label for="confirm_password">confirm_password</label>
+              <input type="confirm_password"id="confirm_password"
+              autocomplete="off"
+              required="required" name="confirm_password" value="<?php echo $confirm_password; ?>">
+       </div>
 
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
-
-
-
-
-
-
+     
+      
+        <button type="submit" class="button1" value="update-reg" name="reg_update">Update Registration</button>
+    </form>
+</div>
 
 
 <?php
@@ -148,27 +109,25 @@ $confirm_password = $resultData['confirm_password'];
 
 
 
-if (isset($_POST['user_registration'])) 
+if (isset($_POST['update-reg'])) 
 {
 
 	// Escape special characters in a string for use in an SQL statement
-	$id = $_POST['id'];
 
-	$user_name = $_POST['user_name'];
-
-	$email =  $_POST['email'];
-
-	$password =  $_POST['password'];
-	
-	$confirm_password=  $_POST['confirm_password'];
-	
+  $id = $_POST['id'];
 	
 
-		
-	 
-	$sql = "UPDATE user_register SET `user_name` = '$user_name',`email` = '$email', 
-	`password` = '$password',`confirm_password` = '$confirm_password'
-	  WHERE `id` = $id";
+	$user_name=$_POST['user_name'];
+
+	$email = $_POST['description'];
+	
+	$password = $_POST['password'];
+	
+	$confirm_password= $_POST['confirm_password'];
+	
+	$sql = "UPDATE add_product SET    `user_name` = '$user_name', 
+	`email` = '$email',`password` = '$password',
+	`confirm_password` = '$confirm_password'  WHERE `id` = $id";
 		
 if($con->query($sql))
         {
@@ -181,26 +140,95 @@ if($con->query($sql))
         {
             echo    '<script type="text/javascript">
             alert ("Record Not Update");
-            window.location="update-registration.php?id=$id";
+            window.location="update-registration.php"; ;
             </script>';
         }
 }
 
 ?>
-</head>
 
-<!---last child--->
-<div class="bg-info p-3 text-center">
-    Developed by:-<br>
-Gaikwad Rutuja ,
-Nimse Nikita 
+
+
+
+
+
 </div>
+
+
+
+      <!-- Footer -->
+      <div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3 class="footer-title">STARLIGHT CINEMA</h3>
+                <p>Buy movie tickets easily with Starlight Cinema system nationwide</p>
+                <a href="#" class="btn btn-warning">Get Your Ticket</a>
+            </div>
+            <div class="col-md-2">
+                <h5 class="footer-title">Movies</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">Comedy Shows</a></li>
+                    <li><a href="#">Drama</a></li>
+                    <li><a href="#">Kids Movie</a></li>
+                    <li><a href="#">Marathi Movies</a></li>
+                    <li><a href="#">Hindi Movies</a></li>
+                    <li><a href="#">English Movies</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h5 class="footer-title">Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+                </div>
+                
+            <div class="col-md-4">
+                <h5 class="footer-title">Newsletter</h5>
+                <p>Subscribe to STARLIGHT CINEMA .</p>
+                <div class="newsletter">
+                    <input type="email" class="form-control" placeholder="Email Address">
+                    <button class="btn btn-warning mt-2">Subscribe</button>
+                </div>
+                <div class="mt-2">
+                    <input type="checkbox" id="terms">
+                    <label for="terms">I agree to all terms and policies of the company</label>
+                </div>
+                <div class="social-icons mt-3">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="text-center mt-4">
+            <p>&copy; Copyright 2024 by starlightcinema.com  &nbsp; &nbsp; &nbsp; &nbsp;    Developed By Gaikwad Rutuja & Nimse Nikita</p>
+          
+        </div>
+    </div>
 </div>
-<!--bootstarap js link-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">
- </script>
- </body>
-</html>
-      
+
+
+
+<!-- Font Awesome for social media icons -->
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+  <!-- Font Awesome CDN for social media icons -->
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="script.js"></script>
 </body>
 </html>
