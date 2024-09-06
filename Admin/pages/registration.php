3029c38fe769499bf082129695927f8e8ad4f0ea
NikitaNimse <nimsenikita710>
2024-09-06 21:23:34 +0530

    footer

--- Admin/pages/registration.php
+++ Admin/pages/registration.php
@@ -81,64 +81,56 @@
         </form>
     </div>
 
-   <!-- Footer -->
-   <div class="footer">
+  
+     <!-- Footer -->
+     <div class="footer bg-dark text-light py-4">
     <div class="container">
         <div class="row">
             <div class="col-md-4">
-                <h3 class="footer-title">STARLIGHT CINEMA</h3>
-                <p>Buy movie tickets easily with Starlight Cinema system nationwide</p>
-                <a href="#" class="btn btn-warning">Get Your Ticket</a>
-            </div>
-            <div class="col-md-2">
-                <h5 class="footer-title">Movies</h5>
-                <ul class="list-unstyled">
-                    <li><a href="#">Comedy Shows</a></li>
-                    <li><a href="#">Drama</a></li>
-                    <li><a href="#">Kids Movie</a></li>
-                    <li><a href="#">Marathi Movies</a></li>
-                    <li><a href="#">Hindi Movies</a></li>
-                    <li><a href="#">English Movies</a></li>
-                </ul>
-            </div>
-            <div class="col-md-2">
-                <h5 class="footer-title">Links</h5>
-                <ul class="list-unstyled">
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Gallery</a></li>
-                    <li><a href="#">FAQ</a></li>
-                    <li><a href="#">Login</a></li>
-                </ul>
-                </div>
+            <div class="logo"><img src="./../../Images/logo2.png" class="logo-img" alt="Logo"></div>
+               
+                <p style="margin-top:10px;">Experience seamless ticket booking with STARLIGHT CINEMA across the nation. Watch the latest movies in comfort and style.</p>
                 
-            <div class="col-md-4">
-                <h5 class="footer-title">Newsletter</h5>
-                <p>Subscribe to STARLIGHT CINEMA .</p>
-                <div class="newsletter">
-                    <input type="email" class="form-control" placeholder="Email Address">
-                    <button class="btn btn-warning mt-2">Subscribe</button>
-                </div>
-                <div class="mt-2">
-                    <input type="checkbox" id="terms">
-                    <label for="terms">I agree to all terms and policies of the company</label>
-                </div>
-                <div class="social-icons mt-3">
-                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#"><i class="fab fa-twitter"></i></a>
-                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                </div>
             </div>
+            <div class="col-md-2">
+                <h5 class="footer-title">Explore Movies</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-light">Comedy Shows</a></li>
+                    <li><a href="#" class="text-light">Drama</a></li>
+                    <li><a href="#" class="text-light">Kids Movies</a></li>
+                    <li><a href="#" class="text-light">Marathi Movies</a></li>
+                    <li><a href="#" class="text-light">Hindi Movies</a></li>
+                    <li><a href="#" class="text-light">English Movies</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h5 class="footer-title">Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-light">About Us</a></li>
+                    <li><a href="#" class="text-light">Contact Us</a></li>
+                    <li><a href="#" class="text-light">Gallery</a></li>
+                    <li><a href="#" class="text-light">FAQ</a></li>
+                    <li><a href="#" class="text-light">Login</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+    <h5 class="footer-title">Rate Us</h5>
+    <p>We value your feedback! Please rate your experience with us:</p>
+    <div class="rating">
+    <p>Rating: ⭐⭐⭐⭐☆</p>
+    </div>
+    <p>Your rating helps us improve our service!</p>
+</div>
+       
         </div>
-        <div class="text-center mt-4">
-            <p>&copy; Copyright 2024 by starlightcinema.com  &nbsp; &nbsp; &nbsp; &nbsp;    Developed By Gaikwad Rutuja & Nimse Nikita</p>
-          
+        <div class="row mt-3 text-center">
+            <div class="col-12">
+                <p>&copy; 2024 STARLIGHT CINEMA.  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  Developed by Gaikwad Rutuja & Nimse Nikita</p>  
+                 
+            </div>
         </div>
     </div>
 </div>
-
-
 
 <!-- Font Awesome for social media icons -->
 <script src="https://kit.fontawesome.com/a076d05399.js"></script>
@@ -146,18 +138,5 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-
-  <!-- Font Awesome CDN for social media icons -->
-  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script>  document.querySelector('.nav').addEventListener('click', function() {
-            this.querySelector('ul').classList.toggle('show');
-        });</script>
 </body>
 </html>
-
