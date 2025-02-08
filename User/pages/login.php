1058e948fd862b69a82dd7353c5ffbda4d3117d0
rutuja091 <gaikwadrutuja254@gmail.com>
2025-02-08 21:52:54 +0530

    add changes

--- User/pages/login.php
+++ User/pages/login.php
@@ -55,6 +55,7 @@ if (isset($_POST['login'])) {
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
 </head>
 <body>
 
